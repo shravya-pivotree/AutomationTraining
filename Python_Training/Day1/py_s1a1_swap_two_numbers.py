3f2360ae534db64ae11e4b23d18840a471bcaff9
DESKTOP-5LV81OJ\shravya.srinivasan <Shravya.Srinivasan@pivotree.com>
2025-06-19 15:10:26 +0530

    Day 1 commit

---
+++ Python_Training/Day1/py_s1a1_swap_two_numbers.py
@@ -0,0 +1,9 @@
+x=234
+y=523
+print("The wrong locker code of x:",x)
+print("The wrong locker code of y:",y)
+z=x
+x=y
+y=z
+print("The correct locker code of x:",x)
+print("The correct locker code of y:",y) 
